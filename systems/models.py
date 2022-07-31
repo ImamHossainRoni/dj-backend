d797b58aa3f9469ec53dc7ba5c11bff8286089f2
roni <imamhossainroni95@gmail.com>
2022-07-31 21:34:04 +0600

    >> SYS ADD : <*> Base model class added as an abstract class to use in all others classes as a base.

--- systems/models.py
+++ systems/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+import uuid
+
+
+class ActiveManager(models.Manager):
+    """ Only returns active objects"""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(active=True)
+
+
+class InactiveManager(models.Manager):
+    """ Only returns inactive objects"""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(active=False)
+
+
+class BaseModel(models.Model):
+    """
+    Used in all the models as base
+    """
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True,
+                                 blank=True,
+                                 help_text='If this object is active')
+
+    active_objects = ActiveManager()
+    inactive_objects = InactiveManager()
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
