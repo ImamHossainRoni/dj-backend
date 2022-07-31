a6737f9bdfb0f293eb534742819e2d4b0521c463
roni <imamhossainroni95@gmail.com>
2022-07-31 21:57:49 +0600

    >> APP ADD : <Catalogs> ProductImageSource model added.

--- catalogs/models.py
+++ catalogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from systems.models import BaseModel
+
 
 # Create your models here.
+
+class ProductImageSource(BaseModel):
+    """ To store product image source information"""
+
+    source_url = models.URLField(max_length=200,
+                                 help_text='Product image source url where image data will be scrapping')
+    name = models.CharField(max_length=200, null=True, blank=True,
+                            help_text='Image source name, like - `Unsplash`')
+
+    class Meta:
+        db_table = 'product_image_sources'
