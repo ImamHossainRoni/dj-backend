8ed5134515b57b2f2c5024596c3450467abd15ad
roni <imamhossainroni95@gmail.com>
2022-08-01 22:28:02 +0600

    >> APP ADD : <Catalogs> ProductImage model added.

--- catalogs/models.py
+++ catalogs/models.py
@@ -14,3 +14,19 @@ class ProductImageSource(BaseModel):
 
     class Meta:
         db_table = 'product_image_sources'
+
+
+class ProductImage(BaseModel):
+    """ To store image information with metadata"""
+    original_url = models.URLField(max_length=200,
+                                   help_text='Product image original url that can be used to get that specific image')
+
+    original_image = models.ImageField(upload_to='images/', help_text='Actual image')
+    height = models.PositiveIntegerField(default=0, help_text='Actual image height')
+    width = models.PositiveIntegerField(default=0, help_text='Actual image width')
+    title = models.CharField(max_length=200, null=True, blank=True, help_text='Image title')
+    description = models.CharField(max_length=500, null=True, blank=True,
+                                   help_text='Product image description if anything needed else blank')
+
+    class Meta:
+        db_table = 'product_images'
