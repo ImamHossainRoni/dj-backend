63387b4c733a482edcf28271de9c38127a2ec800
roni <imamhossainroni95@gmail.com>
2022-08-02 23:22:10 +0600

    >> SYS ADD : <*> Rest framework permission removed for time being

--- dj_backend/settings.py
+++ dj_backend/settings.py
@@ -103,15 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # REST FRAMEWORK
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
-    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
