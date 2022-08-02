4e8f674e86cc2dfd7f32932d81bfe52fb6bee2b8
roni <imamhossainroni95@gmail.com>
2022-08-02 22:58:12 +0600

    >> APP ADD : <Catalogs> New model Product Image Source added

--- catalogs/models.py
+++ catalogs/models.py
@@ -18,11 +18,26 @@ class ProductImageSource(BaseModel):
 
 class ProductImage(BaseModel):
     """ To store image information with metadata"""
+    source = models.ForeignKey(ProductImageSource,
+                               null=False, blank=False,
+                               on_delete=models.CASCADE,
+                               help_text='That refers to related product image source'
+                               )
     original_url = models.URLField(max_length=200,
                                    help_text='Product image original url that can be used to get that specific image')
 
     original_image = models.ImageField(upload_to='images/', help_text='Actual image')
-    height = models.PositiveIntegerField(default=0, help_text='Actual image height')
+    small_image = models.ImageField(upload_to='images/small/',
+                                    null=True, blank=True,
+                                    help_text='Small image')
+    medium_image = models.ImageField(upload_to='images/medium/',
+                                     null=True, blank=True,
+                                     help_text='Medium image')
+    large_image = models.ImageField(upload_to='images/large/',
+                                    null=True, blank=True,
+                                    help_text='Large image')
+    height = models.PositiveIntegerField(default=0,
+                                         help_text='Actual image height')
     width = models.PositiveIntegerField(default=0, help_text='Actual image width')
     title = models.CharField(max_length=200, null=True, blank=True, help_text='Image title')
     description = models.CharField(max_length=500, null=True, blank=True,
