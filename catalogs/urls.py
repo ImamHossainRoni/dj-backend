6cac19a37b3dc66a56c51ba02a4453892d469fe5
roni <imamhossainroni95@gmail.com>
2022-08-05 22:38:47 +0600

    >> APP FIX : <Catalogs> urls typo issue fixed

--- catalogs/urls.py
+++ catalogs/urls.py
@@ -2,10 +2,9 @@ from django.urls import path, include
 from .views import ProductImageRetrieveAPIView, ProductImageListAPIView, ProductImageRetrieveBySourceAPIView
 
 urlpatterns = [
-    # genders
     path('images/retrieve/<uuid:pk>/', ProductImageRetrieveAPIView.as_view(), name='retrieve_product_image'),
     path('images/retrieve-by-source-url/<str:source_url>/', ProductImageRetrieveBySourceAPIView.as_view(),
-         name='retrieve_product_image_by_source_url'),
+         name='retrieve_product_images_by_source_url'),
     path('images/all/', ProductImageListAPIView.as_view(), name='all_product_images'),
 
 ]
