3c93c8605f4ce3d21887ccf0a971a13197c9c327
roni <imamhossainroni95@gmail.com>
2022-08-05 20:25:30 +0600

    >> APP ADD : <Catalogs> to_representation method override to customize API responses.

--- catalogs/serializers.py
+++ catalogs/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from .models import ProductImage, ProductImageSource
+from .services import get_query_to_image_width
 
 
 class ProductImageSourceSerializer(ModelSerializer):
@@ -15,9 +16,35 @@ class ProductImageSourceSerializer(ModelSerializer):
 class ProductImageSerializer(ModelSerializer):
     source = ProductImageSourceSerializer()
 
+    def to_representation(self, instance):
+        """ API response modified based on some conditions"""
+        request = self.context.get('request')
+        query = request.query_params.get('size')
+
+        image_width = get_query_to_image_width(query)
+
+        data = {
+            'id': instance.id,
+            'title': instance.title,
+            'description': instance.description,
+            'created_at': instance.created_at,
+            'original_image_source_url': instance.original_url,
+            'image_url': request.build_absolute_uri(instance.original_image.url),
+            'previous_height': instance.height,
+            'previous_width': instance.width,
+            'source': ProductImageSourceSerializer(instance.source).data,
+        }
+        if image_width == 256:
+            data['image_url'] = request.build_absolute_uri(instance.small_image.url)
+        if image_width == 1024:
+            data['image_url'] = request.build_absolute_uri(instance.medium_image.url)
+        if image_width == 2048:
+            data['image_url'] = request.build_absolute_uri(instance.large_image.url)
+
+        return data
+
     class Meta:
         model = ProductImage
         exclude = [
-            'created_at',
             'updated_at',
         ]
